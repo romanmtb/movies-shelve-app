dc47e216440025109720cbe5a3f7c9d513203c5d
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-10-04 00:16:52 +0300

    add postman collection

--- src/server/index.js
+++ src/server/index.js
@@ -12,8 +12,9 @@ const app = express();
 const router = express.Router();
 const admin = require('firebase-admin');
 const firebase = require("firebase");
+const PORT = 4125
 
-var config = {
+let config = {
     apiKey: "AIzaSyDmNmhYd8smrolZuwd5W1WsQtsJBh3ddfM",
     authDomain: "react-movies-shelve.firebaseapp.com",
     databaseURL: "https://react-movies-shelve.firebaseio.com",
@@ -23,49 +24,72 @@ var config = {
 };
 
 firebase.initializeApp(config);
-var database = firebase.database();
-var all = database.ref().once('value').then(function (snapshot) {
-  console.log(snapshot.val().movie);
-})
+let database = firebase.database();
 
-const PORT = 4125
+//variable, which holds highest ID
+let LAST_ID = -1;
+
+database.ref().once('value').then(function (snapshot) {
+   LAST_ID = Math.max.apply(null, Object.keys(snapshot.val().movie));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// routes(app);
 // middleware to use for all requests
 router.use(function (req, res, next) {
   // do logging
-  console.log('Something is happening.');
-  next(); // make sure we go to the next routes and don't stop here
+  //console.log('Something is happening.');
+  next();
 });
 
+//DEFAULT ROUTE
 router.get('/', function (req, res) {
   res.json({message: 'hooray! welcome to our api!'});
 });
 
+//ADD
 router.route('/movies').post(function (req, res) {
-  res.json({message: 'movie created!'});
+  LAST_ID++;
+  firebase.database().ref('movie/' + LAST_ID).set(
+    req.body.movie, function(error) {
+    if (error) {
+      res.json({message: 'ERROR'});
+    } else {
+      res.json({message: 'movie created!'});
+      //FIXME #2 add error handling for requests
+    }
+  })
+//GET ALL
 }).get(function (req, res) {
   database.ref().once('value').then(function (snapshot) {
     res.json(snapshot.val().movie);
   })
+  .catch();  //FIXME #2 add error handling for requests
 });
 
+//GET BY ID
 router.route('/movies/:movieId')
   .get(function (req, res) {
     res.json({message: `${req.params.movieId} get this movie`});
   })
+  //UPDATE
   .put(function (req, res) {
-    res.json({message: `${req.params.movieId} update this movie`});
+    firebase.database().ref('movie/' + req.params.movieId).update(req.body.movie).then(i =>
+      res.json({message: `${req.params.movieId} update this movie`})
+    )
+      .catch();  //FIXME #2 add error handling for requests
   })
-  .delete(function (req, res) {
-    res.json({message: `${req.params.movieId} movie deleted`});
+  //DELETE
+  .delete(function(req, res) {
+    firebase.database().ref('movie/' + req.params.movieId).remove()
+      .then(i =>
+        res.json({message: `${req.params.movieId} movie deleted`})
+      )
+      .catch(); //FIXME #2 add error handling for requests
   });
 
 app.use('/api', router);
-
 
 const server = app.listen(PORT, function () {
   console.log("app running on port.", server.address().port);
