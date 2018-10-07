ec228cf8716c791d0ce3e19ed9610c34468a92af
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-10-07 03:05:20 +0300

    update card grid

--- src/client/components/HeaderComponent.js
+++ src/client/components/HeaderComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderComponent = () => {
   return (
-    <div className="jumbotron col-lg-12">
+    <div className="jumbotron bg-secondary text-white col-lg-12">
       <h1 className="display-4">
         MOVIE SHELTER <small className="lead">Test task</small>
       </h1>
--- src/client/components/MovieCardComponent.js
+++ src/client/components/MovieCardComponent.js
@@ -15,32 +15,39 @@ class MovieCardComponent extends Component {
   };
 
   render() {
-    const filmStars = this.state.isOpen && <p>{this.props.stars}</p>;
+    const filmStars = this.state.isOpen && (
+      <p className="card-text">{this.props.stars}</p>
+    );
 
     return (
-      <div className="movie-dashboard__card card" style={{ margin: '10px' }}>
-        <div className="card-body">
-          <button
-            className="btn btn-outline-dark"
-            style={{ margin: '4px' }}
-            onClick={() => this.props.deleteHandler(this.props.id)}
-          >
-            DELETE
-          </button>
+      <div className="col-lg-4">
+        <div
+          className="movie-dashboard__card card"
+          style={{ marginBottom: '60px' }}
+        >
+          <div className="card-body">
+            <button
+              className="btn btn-outline-dark"
+              style={{ margin: '10px' }}
+              onClick={() => this.props.deleteHandler(this.props.id)}
+            >
+              DELETE
+            </button>
 
-          <button
-            className="btn btn-outline-dark"
-            style={{ margin: '4px' }}
-            onClick={this.handleClick}
-          >
-            {this.state.isOpen ? 'CLOSE' : 'OPEN'}
-          </button>
+            <button
+              className="btn btn-outline-dark"
+              style={{ margin: '10px' }}
+              onClick={this.handleClick}
+            >
+              {this.state.isOpen ? 'CLOSE' : 'OPEN'}
+            </button>
 
-          <h6>ID {this.props.id}</h6>
-          <h5>{this.props.title}</h5>
-          <h6>{this.props.release}</h6>
-          <h6>{this.props.format}</h6>
-          {filmStars}
+            <h5 className="card-title">{this.props.title}</h5>
+            <h6>ID {this.props.id}</h6>
+            <h6 className="card-text">{this.props.release}</h6>
+            <h6 className="card-text">{this.props.format}</h6>
+            {filmStars}
+          </div>
         </div>
       </div>
     );
